09243b3c2879e8d8d85b78af307431247d23f8ac
WoohyunCha <woohyun321@snu.ac.kr>
2025-11-20 14:24:20 +0900

    stiffer gain is default, high com is default

--- legged_gym/legged_gym/envs/igris_c/igris_c_config.py
+++ legged_gym/legged_gym/envs/igris_c/igris_c_config.py
@@ -15,18 +15,18 @@ class IGRISCCfg( LeggedRobotCfg ):
     class init_state(LeggedRobotCfg.init_state):
         default_joint_angles = {
             # Left leg (6)
-            "Joint_Hip_Pitch_Left": -0.45,
+            "Joint_Hip_Pitch_Left": -0.2,
             "Joint_Hip_Roll_Left": 0.0,
             "Joint_Hip_Yaw_Left": 0.,
-            "Joint_Knee_Pitch_Left": 0.75,
-            "Joint_Ankle_Pitch_Left": -0.38,
+            "Joint_Knee_Pitch_Left": 0.3,
+            "Joint_Ankle_Pitch_Left": -0.15,
             "Joint_Ankle_Roll_Left": 0.0,
             # Right leg (6)
-            "Joint_Hip_Pitch_Right": -0.45,
+            "Joint_Hip_Pitch_Right": -0.2,
             "Joint_Hip_Roll_Right": 0.0,
             "Joint_Hip_Yaw_Right": 0.0,
-            "Joint_Knee_Pitch_Right": 0.75,
-            "Joint_Ankle_Pitch_Right": -0.38,
+            "Joint_Knee_Pitch_Right": 0.3,
+            "Joint_Ankle_Pitch_Right": -0.15,
             "Joint_Ankle_Roll_Right": 0.0,
             # Waist + Neck (5)
             # "Joint_Waist_Yaw": 0.0,
@@ -56,23 +56,23 @@ class IGRISCCfg( LeggedRobotCfg ):
     class control(LeggedRobotCfg.control):
         stiffness = {
             # Left leg (6)
-            "Joint_Hip_Pitch_Left": 100,
-            "Joint_Hip_Roll_Left": 100,
-            "Joint_Hip_Yaw_Left": 50.0,
-            "Joint_Knee_Pitch_Left": 100,
-            "Joint_Ankle_Pitch_Left": 41.1,
-            "Joint_Ankle_Roll_Left": 34.166,
+            "Joint_Hip_Pitch_Left": 150,
+            "Joint_Hip_Roll_Left": 150,
+            "Joint_Hip_Yaw_Left": 100.0,
+            "Joint_Knee_Pitch_Left": 150,
+            "Joint_Ankle_Pitch_Left": 80.181,
+            "Joint_Ankle_Roll_Left": 65.6563,
             # Right leg (6)
             "Joint_Hip_Pitch_Right": 100,
             "Joint_Hip_Roll_Right": 100,
             "Joint_Hip_Yaw_Right": 50.0,
             "Joint_Knee_Pitch_Right": 100,
-            "Joint_Ankle_Pitch_Right": 41.1,
-            "Joint_Ankle_Roll_Right": 34.166,
+            "Joint_Ankle_Pitch_Right": 80.181,
+            "Joint_Ankle_Roll_Right": 65.6563,
             # Waist + Neck (5)
             # "Joint_Waist_Yaw": 100.0,
-            "Joint_Waist_Roll": 163.63,
-            "Joint_Waist_Pitch": 42.863,
+            "Joint_Waist_Roll": 458.206,
+            "Joint_Waist_Pitch": 137.664,
             # "Joint_Neck_Yaw": 30.0,
             # "Joint_Neck_Pitch": 30.0,
             # # Left arm (7)
@@ -94,23 +94,23 @@ class IGRISCCfg( LeggedRobotCfg ):
         }
         damping = {
             # Left leg (6)
-            "Joint_Hip_Pitch_Left": 1.5,
-            "Joint_Hip_Roll_Left": 2.5,
-            "Joint_Hip_Yaw_Left": 1.2,
-            "Joint_Knee_Pitch_Left": 0.75,
-            "Joint_Ankle_Pitch_Left": 2.7948,
-            "Joint_Ankle_Roll_Left": 2.323,
+            "Joint_Hip_Pitch_Left": 2.,
+            "Joint_Hip_Roll_Left": 3.,
+            "Joint_Hip_Yaw_Left": 1.5,
+            "Joint_Knee_Pitch_Left": 1.,
+            "Joint_Ankle_Pitch_Left": 3.5284,
+            "Joint_Ankle_Roll_Left": 2.93288,
             # Right leg (6)
-            "Joint_Hip_Pitch_Right": 1.5,
-            "Joint_Hip_Roll_Right": 2.5,
-            "Joint_Hip_Yaw_Right": 1.2,
-            "Joint_Knee_Pitch_Right": 0.75,
-            "Joint_Ankle_Pitch_Right": 2.7948,
-            "Joint_Ankle_Roll_Right": 2.323,
+            "Joint_Hip_Pitch_Right": 2.,
+            "Joint_Hip_Roll_Right": 3.,
+            "Joint_Hip_Yaw_Right": 1.5,
+            "Joint_Knee_Pitch_Right": 1.,
+            "Joint_Ankle_Pitch_Right": 3.5284,
+            "Joint_Ankle_Roll_Right": 2.93288,
             # # Waist + Neck (5)
             # "Joint_Waist_Yaw": 2.0,
-            "Joint_Waist_Roll": 7.8544,
-            "Joint_Waist_Pitch": 2.0574,
+            "Joint_Waist_Roll": 11.7824,
+            "Joint_Waist_Pitch": 3.53993,
             # "Joint_Neck_Yaw": 1.0,
             # "Joint_Neck_Pitch": 1.0,
             # # Left arm (7)
@@ -198,10 +198,10 @@ class IGRISCCfg( LeggedRobotCfg ):
         ]
 
     class rewards( LeggedRobotCfg.rewards ):
-        base_height_target = 0.93
+        base_height_target = 0.95
         soft_dof_pos_limit = .95 # percentage of urdf limits, values above this limit are penalized
         only_positive_rewards = False # if true negative total rewards are clipped at zero (avoids early termination problems)
-        clearance_height_target = -0.78
+        clearance_height_target = -0.8
         class scales:
             termination = -200.
             tracking_lin_vel = 5.0
